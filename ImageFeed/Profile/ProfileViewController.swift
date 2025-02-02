75c506821c5948eedc62741d395b950b6f02fe50
Симонов Иван дмитриевич <fensiz@me.com>
2025-02-02 18:02:54 +0300

    добален tabbar + profile

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Симонов Иван Дмитриевич on 02.02.2025.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+	@IBOutlet private var avatarImageView: UIImageView!
+	@IBOutlet private var nameLabel: UILabel!
+	@IBOutlet private var loginNameLabel: UILabel!
+	@IBOutlet private var descriptionLabel: UILabel!
+
+	@IBOutlet private var logoutButton: UIButton!
+
+	@IBAction private func didTapLogoutButton() {
+	}
+}
